f2affd7577a85be94e1a7c8538d2d95856c73cca
Tom Feltwell <tfeltwell@gmail.com>
2014-12-16 23:02:28 +0000

    Implements main framework for pygame (very rough). Will work on backend most, then do the UI

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import pygame, sys
+from pygame.locals import *
 import daySystem, demander
 
 if __name__ == "__main__":
+	pygame.init()
+	DISPLAYSURF = pygame.display.set_mode((800,600))
+	pygame.display.set_caption("Sussex Smuggler")
+	splash = pygame.image.load("splash.jpg")
+	splashrect = splash.get_rect()
+
 	calendar = daySystem.daySystem()
 	demand = demander.demander(calendar)
 	print 'Started'
+	frameCount = 0
 
-	while calendar.year != 1650:
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+
+		if frameCount >= 1000000:
+			DISPLAYSURF.blit(splash,splashrect)
+			pygame.display.update()
 			calendar.update()
 			demand.update()
-			raw_input() # This is just to manually step through the day
-			print "-----"
+			frameCount = 0
+		else:
+			frameCount += 1
