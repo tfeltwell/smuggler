17df87ed8fa8d77ee32dd62e03c4d61de0111276
Tom Feltwell <tfeltwell@gmail.com>
2014-12-16 22:04:03 +0000

    System is more refined in code terms. Products will randomly increase until reaching threshold. The price of the item is based (tightly) on demand.

--- demander.py
+++ demander.py
@@ -7,30 +7,31 @@ class demander:
 		self.calSystem = calendar # Handle on the calendar object
 
 		# Items and their demand
-		self.cheese = 0
-		self.brandy = 0
-		self.gin = 0
-		self.tobacco = 0
-		self.tea = 0
-		self.wool = 0
+		self.goods = ["cheese","brandy","gin","tobacco","tea","wool"]
+		self.demands = [0,0,0,0,0,0]
+		self.basePrice = [1.0,3.0,5.0,8.0,7.0,15.0]
+		self.price = [1.0,3.0,5.0,8.0,7.0,15.0]
+
+		# Demand system vars
+		self.maxDemand = 100
 
 	def update(self):
-		product = random.randint(0,6)
-		print "product",product
-		self.calSystem.printDate()
-		# increaseDemand(product)
+		product = random.randint(0,5)
+		self.increaseDemand(product)
+		self.updatePrice(product)
 
 	def increaseDemand(self,product):
-		if product == 0:
-			print "Demand for cheese increased"
-		elif product == 1:
-			print "Demand for brandy increased"
-		elif product == 2:
-			print "Demand for gin increased"
-		elif product == 3:
-			print "Demand for tobacco increased"
-		elif product == 4:
-			print "Demand for tea increased"
-		elif product == 5:
-			print "Demand for wool increased"
+		if self.demands[product] is not self.maxDemand:
+			self.demands[product] += 1
+			print "Demand for",self.goods[product],"increased"
+		self.printDemands()
+
+	def updatePrice(self, product):
+		print "Calculated price:",str((float(self.demands[product])/100)+1*self.basePrice[product]),"base price",self.basePrice[product],"demand",self.demands[product]
+		# self.price[product] = (float(self.demands[product])/100)+1*self.basePrice[product]
+
+	def printDemands(self):
+		print "\nToday's demands are as follows:"
+		for i in range(len(self.goods)):
+			print str(self.goods[i]) + ": " + str(self.demands[i])
 
