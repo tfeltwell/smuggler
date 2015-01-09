a86cc82f6049785fe47e125c18aef74ad878ccd0
Tom Feltwell <tfeltwell@gmail.com>
2015-01-09 16:12:05 +0000

    Goods Class This should be implemented further so that every good that is purchased & create is an item of this.
    Probably needs more variables adding to it to make it a bit more flexible.

---
+++ good.py
@@ -0,0 +1,18 @@
+
+class good:
+	def __init__(self, name, basePrice):
+		self.name = name
+		self.basePrice = basePrice
+		self.currentPrice = currentPrice
+
+	def update(self):
+		# update current price here
+
+	def getName(self):
+		return self.name
+
+	def getBasePrice(self):
+		return self.basePrice
+
+	def getCurrentPrice(self):
+		return self.currentPrice
