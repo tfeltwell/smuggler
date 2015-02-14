02c589e4f8d7167f870e981544038ab064e9eb94
Tom Feltwell <tfeltwell@gmail.com>
2015-02-14 14:58:51 +0000

    Character stuff

---
+++ character.py
@@ -0,0 +1,18 @@
+
+class character:
+	def __init__(self, name, profession, home):
+		self.name = name
+		self.profession = profession
+		self.home = home
+
+	def update():
+		print "Updating character"
+
+	def getName(self):
+		return self.name
+
+	def getProfession(self):
+		return self.profession
+
+	def getHome(self):
+		return self.home
